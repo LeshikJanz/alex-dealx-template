1f1ca643c728a1f25eaf19dd341697040b979798
leshikjanz <alex.tereshko@softeq.com>
2018-01-13 14:56:49 +0300

    feat: implement autoSuggestion for search input

--- src/app/sagas/productsSaga.js
+++ src/app/sagas/productsSaga.js
@@ -1,5 +1,6 @@
 import { call, put, fork, takeLatest } from 'redux-saga/effects';
 import * as actions from 'actions/index';
+import * as templateActions from 'template/actions/index';
 import * as api from 'api/index';
 
 import * as constants from 'constants/index';
@@ -44,11 +45,32 @@ function* postProductRequest(req) {
   }
 }
 
+function* handleAutoSuggestion(product) {
+
+}
+
 function* getProductSuggestRequest(req) {
   const { payload: { onComplete, ...search } } = req;
   try {
     const data = yield call(api.getProductSuggest.bind(null, search));
     yield put(actions.getProductSuggestSuccess({ data }));
+
+    const suggestedMake =
+      data[0].Make.find(({ Make }) =>
+      Make.toLowerCase() === search.search.toLowerCase());
+    const suggestedModel =
+      data[0].Model.find(({ Make, Model }) =>
+      search.search.toLowerCase() === `${Make} ${Model}`.toLowerCase());
+    const autoSuggestion = suggestedMake || suggestedModel;
+
+    if (autoSuggestion) {
+      yield put(templateActions.updateProductSearch({
+        make: autoSuggestion.Make,
+        model: autoSuggestion.Model,
+        level: autoSuggestion.level || 1,
+        isSearchable: true
+      }))
+    }
     onComplete();
   } catch (e) {
     yield put(actions.getProductSuggestFailure({ message: 'Cant get products' }));
