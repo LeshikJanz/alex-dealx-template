8a6953cd06b29af54367452dc7d5a990e9608393
leshikjanz <alex.tereshko@softeq.com>
2018-01-13 15:07:39 +0300

    feat: add autoSuggestion for search input

--- src/app/sagas/productsSaga.js
+++ src/app/sagas/productsSaga.js
@@ -45,10 +45,6 @@ function* postProductRequest(req) {
   }
 }
 
-function* handleAutoSuggestion(product) {
-
-}
-
 function* getProductSuggestRequest(req) {
   const { payload: { onComplete, ...search } } = req;
   try {
